cbf06d248d670430d2cd290c373f46fe9217cfce
Lyndsey412 <111444654+Lyndsey412@users.noreply.github.com>
2022-08-30 16:53:46 -0400

    confusion

--- game.py
+++ game.py
@@ -1,24 +1,24 @@
-# from player import Player
-# from ai import AI
-# from human import Human
+from player import Player
+from ai import AI
+from human import Human
 
-# class Game:
-#    def __init__(self,name):
-#     super().__init__(name)
-#     self.get_human_player_choice()
-
-
-# def welcome_screen():
-#     print('Welcome to Rock Paper Scissors Lizard Spock\n \nEach match will be best of three games\nUse the number keys to enter your selection')
+class Game:
+   def __init__(self,name):
+    super().__init__(name)
+    self.get_human_player_choice()
 
 
-# def rules_explained():
-#     print('The rules are as follows:\n Rock crushes Scissors\n Scissors cuts Paper \n Paper covers Rock\n Rock crushes Lizard\n Lizard poisons Simple\n Spock smashes Scissors\n Scissors decapitates Lizard\n Lizard eats Paper\n Paper disproves Spock\n Spock vaporizes Rock\n The winner will win the best of three rounds.')
+def welcome_screen():
+    print('Welcome to Rock Paper Scissors Lizard Spock\n \nEach match will be best of three games\nUse the number keys to enter your selection')
 
 
-# def game_mode():
-#     player_choice = input('Would you like Single or Multi-player?')
-#     print
+def rules_explained():
+    print('The rules are as follows:\n Rock crushes Scissors\n Scissors cuts Paper \n Paper covers Rock\n Rock crushes Lizard\n Lizard poisons Simple\n Spock smashes Scissors\n Scissors decapitates Lizard\n Lizard eats Paper\n Paper disproves Spock\n Spock vaporizes Rock\n The winner will win the best of three rounds.')
+
+
+def game_mode():
+    player_choice = input('Would you like Single or Multi-player?')
+    print
 
     #choose gesture
     #choose game mode
--- player.py
+++ player.py
@@ -1,31 +1,31 @@
 
-# class Player:
-#     def __init__(self):
-#         super().__init__()
-#         pass
+class Player:
+    def __init__(self):
+        super().__init__()
+        pass
 
 
-#     def player_one():
-#         pass
+    def player_one():
+        pass
 
-#     def player_two():
-#         pass
+    def player_two():
+        pass
 
-#     def get_human_player_choice(choice):
-#         choice = int(input('Choose your weapon: '))
-#         while True:
-#             if choice > 5:
-#                 print("Invalid choice. Please choose again")
-#             elif choice < 1:
-#                 print("Invalid choice. Please choose again")
-#             elif choice == 1:
-#                 print("You picked Rock")
-#             elif choice == 2:
-#                 print("You picked Paper")
-#             elif choice == 3:
-#                 print("You picked Scissors")
-#             elif choice == 4:
-#                 print("You picked Lizard")
-#             elif choice == 5:
-#                 print("You picked Spock")
-#             return choice
+    def get_human_player_choice(choice):
+        choice = int(input('Choose your weapon: '))
+        while True:
+            if choice > 5:
+                print("Invalid choice. Please choose again")
+            elif choice < 1:
+                print("Invalid choice. Please choose again")
+            elif choice == 1:
+                print("You picked Rock")
+            elif choice == 2:
+                print("You picked Paper")
+            elif choice == 3:
+                print("You picked Scissors")
+            elif choice == 4:
+                print("You picked Lizard")
+            elif choice == 5:
+                print("You picked Spock")
+            return choice
