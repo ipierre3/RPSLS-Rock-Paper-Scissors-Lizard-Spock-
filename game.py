525981574e5c54b751d913afcfd08b60cdf0c451
Ignace Pierre <ipierre354@gmail.com>
2022-08-30 13:30:37 -0400

    added new to 'game.py'

--- game.py
+++ game.py
@@ -1,6 +1,11 @@
-from player import Player
-from ai import AI
-from human import Human
+# from player import Player
+# from ai import AI
+# from human import Human
 
-class Game:
-    pass
+# class Game:
+#     pass
+
+def welcome_screen():
+    print('Welcome to Rock Paper Scissors Lizard Spock\n \nEach match will be best of three games\nUse the number keys to enter your selection')
+
+welcome_screen()
--- player.py
+++ player.py
@@ -2,8 +2,21 @@ class Player:
     def __init__(self):
         pass
 
-    def player_one():
-        pass
-
-    def player_two():
-        pass
+    def gethumanPlayerChoice():
+        choice = int(input('Choose your weapon: '))
+        while True:
+            if choice > 5:
+                print("Invalid choice. Please choose again")
+            elif choice < 1:
+                print("Invalid choice. Please choose again")
+            elif choice == 1:
+                print("You picked Rock")
+            elif choice == 2:
+                print("You picked Paper")
+            elif choice == 3:
+                print("You picked Scissors")
+            elif choice == 4:
+                print("You picked Lizard")
+            elif choice == 5:
+                print("You picked Spock")
+            return choice
