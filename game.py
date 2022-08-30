5d4ca54a0b09a5c0c72c18a1efa858bd718e70be
Ignace Pierre <ipierre354@gmail.com>
2022-08-30 16:24:51 -0400

    Feat: merging remote repo to local repo

--- game.py
+++ game.py
@@ -1,11 +1,25 @@
-# from player import Player
-# from ai import AI
-# from human import Human
+from player import Player
+from ai import AI
+from human import Human
+from time import sleep
 
-# class Game:
-#     pass
+class Game:
+    pass
 
 def welcome_screen():
-    print('Welcome to Rock Paper Scissors Lizard Spock\n \nEach match will be best of three games\nUse the number keys to enter your selection')
+    print("""\nWelcome to Rock Paper Scissors Lizard Spock\n \nEach match will be best of three games\nUse the number keys to enter your selection\n
+Rock crushes Scissors
+Scissors cuts Paper 
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitates Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock\n""")
 
 welcome_screen()
+
+def number_of_players():
+    print('Choose number ')
